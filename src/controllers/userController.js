87aa944fd3f55e2b2d87ce0c5bfb06bb657ec16d
WaelS87 <wfakhrji@gmail.com>
2022-08-30 18:37:06 -0300

    vista de terminos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,7 @@
+const {loadUsers,storeUsers} = require('../data/usersModule')
+//const {validationResult} =require('express-validator');
+
+
 module.exports = {
     login : (req,res) => {
         return res.render("users/login", {
@@ -8,6 +12,26 @@ module.exports = {
         return res.render("users/registrar", {
             title : "Registro"
         })
+    },
+    condiciones : (req,res)=>{
+        return res.render('users/condiciones',{
+            title : 'condiciones'
+        })
+    },
+    registerNuevo : (req,res)=>{
+       
+        const {fullname,email,password} = req.body;
+        let users = loadUsers()
+        let userNew = {
+            id : users.length > 0 ? users[users.length - 1].id + 1 : 1,
+            fullname : fullname.trim(),
+            email : email.trim(),
+            password : password
+
+        }
+        let userModify = [...users,userNew]
+        storeUsers(userModify);
+        return res.redirect('/users/login')
     },
     profile : (req,res) => {
         return res.render("users/profile", {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-const { login, register, profile, adminProfile } = require("../controllers/userController")
+const { login, register, profile, adminProfile,condiciones,registerNuevo } = require("../controllers/userController")
 
 /* GET users listing. */
 router
   .get('/login', login)
   .get('/register', register)
+  .post('/register',registerNuevo)
   .get("/profile", profile)
   .get("/adminProfile", adminProfile)
+  .get('/condiciones',condiciones)
+  
 
 module.exports = router;
