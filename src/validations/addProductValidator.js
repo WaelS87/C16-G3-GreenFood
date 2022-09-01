26475e1cc7ab4463c2332adec6eae219001d116f
EzeD03 <ezedrojas03@gmail.com>
2022-09-01 02:51:09 -0300

    validaciones en agregar producto terminadas

--- src/validations/addProductValidator.js
+++ src/validations/addProductValidator.js
@@ -2,9 +2,24 @@ const { check } = require("express-validator");
 
 module.exports = [
     check("title")
-    .notEmpty().withMessage("Debes introducir un nombre para el producto").bail()
-    .isLength({
+        .notEmpty().withMessage("Debes introducir un nombre para el producto.").bail()
+        .isLength({
         min : 5,
         max : 25
-    }).withMessage("El nombre debe tener un largo de entre 5 a 25 letras")
+    }).withMessage("El nombre debe tener un largo de entre 5 a 25 letras."),
+    check("category")
+        .notEmpty().withMessage("La categoría no puede estar vacía."),
+    check("price")
+        .notEmpty().withMessage("Debe determinar un precio.").bail()
+        .isInt({
+            min : 1
+        }).withMessage("No puedes introducir un precio menor a 1."),
+    check("discount")
+        .isInt().withMessage("No puedes introducir números negativos."),
+    check("description")
+        .notEmpty().withMessage("Debes dar una descripción al producto.").bail()
+        .isLength({
+            min : 10,
+            max : 225
+        }).withMessage("La descripción debe tener un largo de entre 10 a 225 letras.")
 ]
