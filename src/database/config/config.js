29a698131a89df6e4cd77e862c6aa8a5605e73f7
“mi <nataliasaxo@gmail.com>
2022-10-01 10:51:53 -0300

    config inicial sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,14 @@
-{
+require("dotenv").config();
+
+module.exports= {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    development: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "mysql",
   },
   "test": {
     "username": "root",
