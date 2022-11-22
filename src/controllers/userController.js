ef450b9fba91e1982d6134cbf98fab7b2aba4c49
“mi <nataliasaxo@gmail.com>
2022-11-22 10:30:50 -0300

    cambios userControler profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -82,7 +82,7 @@ module.exports = {
 
     profile : (req,res) => {
         db.User.findByPk(req.params.id,{
-            include:['images']
+            include:['images'] 
         })
         .then((user)=>{
             return res.render("users/profile", {
