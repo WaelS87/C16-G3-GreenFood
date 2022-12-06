55706a59fefaaafb903da13decedd2a6b74df0f7
WaelS87 <wfakhrji@gmail.com>
2022-12-06 17:03:00 -0300

    metrics

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -1,105 +1,95 @@
-const db = require('../../database/models');
+const db = require("../../database/models");
 module.exports = {
-   
-    list: async (req, res) => {
-      try {
-        let products = await db.Product.findAll({
-          order: ["name"],
-          attributes: {
-            exclude: ["created_at", "updated_at"],
+  list: async (req, res) => {
+    try {
+      const {rows:products,count} = await db.Product.findAndCountAll({
+        order: ["name"],
+        attributes: {
+          exclude: ["created_at", "updated_at"],
+        },
+      });
+      if (products.length) {
+        return res.status(200).json({
+          ok: true,
+          meta: {
+            total: count,
           },
-        });
-        if (products.length) {
-          return res.status(200).json({
-            ok: true,
-            meta: {
-              total: products.length,
-            },
-            data: products,
-          });
-        }
-        throw new Error({
-          ok: false,
-          msg: "hubo un error",
-        });
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-          ok: false,
-          msg: error.message
-            ? error.message
-            : "comunica con elcontralador del sitio"
+          data: products
         });
       }
-    },
-    total : async (req,res) => {
-      try {
+      throw new Error({
+        ok: false,
+        msg: "hubo un error",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        ok: false,
+        msg: error.message
+          ? error.message
+          : "comunica con elcontralador del sitio",
+      });
+    }
+  },
+ /* total: async (req, res) => {
+    try {
+      const total = await db.Product.count();
 
-          const totalProducts = await db.Product.count();
-         
-
-          return res.status(200).json({
-              ok : true,
-              data : {
-                  
-                  totalProducts,
-                 
-              }
-          })
-
-          
-      } catch (error) {
-          return res.status(error.status || 500).json({
-              ok : false,
-              msg : error.message || 'Comunicate con el administrador'
-          })
-      }},
-    detail: async (req, res) => {
-        /* OPTIONS DEFAULT */
-        let options = {
-          include: [
-            {
-              association: "images",
-              attributes: {
-              
-                exclude: ["createdAt", "updatedAt", "deletedAt", "productId"],
-              
-              },
-            },
-            {
-              association: "category",
-              attributes: {
-                exclude: ["createdAt", "updatedAt", "deletedAt"],
-              },
-            },
-          ],
+      return res.status(200).json({
+        ok: true,
+        data: {
+          total
+        },
+      });
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok: false,
+        msg: error.message || "Comunicate con el administrador",
+      });
+    }
+  },*/
+  detail: async (req, res) => {
+    /* OPTIONS DEFAULT */
+    let options = {
+      include: [
+        {
+          association: "images",
           attributes: {
-            exclude: ["updatedAt", "deletedAt", "createdAt"],
+            exclude: ["createdAt", "updatedAt", "deletedAt", "productId"],
           },
-        };
-    
-        try {
-          const idProduct = req.params.id;
-    
-          if (isNaN(idProduct)) {
-            return sendJsonError("El parámetro es invalido", res);
-          }
-    
-          const product = await db.Product.findByPk(idProduct, options);
-    
-          if (!product) {
-            return sendJsonError("El producto solicitado no existe", res, 404);
-          }
-    
-          return res.status(200).json({
-            ok: true,
-            status: 200,
-            data: product,
-          });
-        } catch (error) {
-          sendJsonError(error, res);
-        }
+        },
+        {
+          association: "category",
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "deletedAt"],
+          },
+        },
+      ],
+      attributes: {
+        exclude: ["updatedAt", "deletedAt", "createdAt"],
+      },
+    };
+
+    try {
+      const idProduct = req.params.id;
+
+      if (isNaN(idProduct)) {
+        return sendJsonError("El parámetro es invalido", res);
       }
-       
-      
-}
+
+      const product = await db.Product.findByPk(idProduct, options);
+
+      if (!product) {
+        return sendJsonError("El producto solicitado no existe", res, 404);
+      }
+
+      return res.status(200).json({
+        ok: true,
+        status: 200,
+        data: product,
+      });
+    } catch (error) {
+      sendJsonError(error, res);
+    }
+  },
+};
