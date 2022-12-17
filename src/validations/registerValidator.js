10724892b1db6c0c3544829c385423e14f927254
WaelS87 <wfakhrji@gmail.com>
2022-12-17 15:54:46 -0300

    email registrado

--- src/validations/registerValidator.js
+++ src/validations/registerValidator.js
@@ -18,30 +18,25 @@ module.exports=[
         /* .isAlpha().withMessage('Por Favor ingrése Tu Apellido Bien').bail() */
         .isLength({min:3}).withMessage('El apellido ingresado no es válido'),
 
-    body('email').toLowerCase()
-        .notEmpty().withMessage('Debe ingresar una dirección email').bail()
-        .isEmail().withMessage('El email ingresado no es válido').bail()
-        .custom((value,{req})=> {
-            let user = db.User.findOne({
-                where : {
-                    email : value
-                }
-            })
-            .then(user=>{
-                if(user == null){
-                    return false
-                }else{
-                    return true
-                }
-                
-                
-            
-            })
-            .catch(error=>console.log(error))
-           
-            return !!user
-            
-        }).withMessage('el mail ya esta registrado'),
+  
+    body("email")
+        .notEmpty()
+        .withMessage("*El email es obligatorio")
+        .bail()
+        .isEmail()
+        .withMessage("*Debe ser un email válido"),
+    
+      body("email").custom((value) => {
+        return db.User.findOne({
+          where: {
+            email: value,
+          },
+        }).then((user) => {
+          if (user) {
+            return Promise.reject("Este email ya está registrado");
+          }
+        });
+      }),
 
       
     
